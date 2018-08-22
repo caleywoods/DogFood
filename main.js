001988d703ee8c91b3588b6b91c3d96125ac10ea
Caley Woods <caley@Caleys-MBP.attlocal.net>
2018-08-22 00:07:47 -0500

    Fix row parsing to handle new infomation

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ function getMeals() {
     let rows       = $( '.tableOddRow, .tableEvenRow' ).toArray();
     let re         = /[\n\r]+/;
     let mealEvents = [];
+    let latestBalance;
 
-    rows.forEach((row) => {
+    rows.forEach((row, idx) => {
       /* This splits row via the carriage return which gives us a 2 element
        * array in the form of:
        * ["Mon	08/28/17	", "1.15	0.00	1.45	0.00	0.00	0.00	6.00	2.60	3.40	6.20"]
@@ -17,9 +18,9 @@ function getMeals() {
        *
        * index 1 contains the date we need for moment
        */
-      let mealDate = rowArr[0].split('	')[1];
+      let mealDate = moment(rowArr[0].split('	')[1]);
 
-      /* This breaks the second element into a 10 element array in the form of:
+      /* This breaks the second element into a 12 element array in the form of:
        * ["1.15", "0.00", "1.45", "0.00", "0.00", "0.00", "6.00", "2.60", "3.40", "6.20"]
        *
        * [0] is breakfast meal charge
@@ -28,44 +29,36 @@ function getMeals() {
        * [3] is lunch a la carte (not available until High School)
        * [4] is snack meal charge (not used past pre school?)
        * [5] is snack a la carte ( not used past pre school?)
-       * [6] is deposits
-       * [7] is total purchases (on the day)
+       * [6] is Total Daily Charge
+       * [7] is deposits
        * [8] is net (don't care)
-       * [9] is balance
+       * [9] is the word "Charge" (useless)
+       * [10] is daily balance
+       * [11] is an empty string
        */
       let transactionDetails = rowArr[1].split('	'); //fuck your tab character.
 
       let breakfastCharge = Number( transactionDetails[0] );
       let lunchCharge     = Number( transactionDetails[2] );
-      let deposit         = Number( transactionDetails[6] );
-      let dailyBalance    = Number( transactionDetails[9] );
+      let deposit         = Number( transactionDetails[7] );
+      let dailyBalance    = Number( transactionDetails[10] );
 
       if ( breakfastCharge > 0 ) {
         let breakfastEvent = {};
-        breakfastEvent.title = "Breakfast - $" + breakfastCharge.toFixed( 2 );
-        breakfastEvent.start = moment( mealDate );
-        breakfastEvent.end   = moment( mealDate );
-
-        breakfastEvent.start.hour( 7 );
-        breakfastEvent.start.minutes( 30 );
-
-        breakfastEvent.end.hour( 8 );
-        breakfastEvent.end.minutes( 00 );
+        breakfastEvent.title = `Breakfast - $${breakfastCharge.toFixed( 2 )}`;
+        breakfastEvent.allDay = true;
+        breakfastEvent.start = mealDate;
+        breakfastEvent.end   = mealDate;
 
         mealEvents.push( breakfastEvent );
       }
 
       if ( lunchCharge > 0 ) {
           let lunchEvent = {};
-          lunchEvent.title = "Lunch - $" + lunchCharge.toFixed( 2 );
-          lunchEvent.start = moment( mealDate );
-          lunchEvent.end   = moment( mealDate );
-
-          lunchEvent.start.hour( 11 );
-          lunchEvent.start.minutes( 30 );
-
-          lunchEvent.end.hour( 12 );
-          lunchEvent.end.minutes( 00 );
+          lunchEvent.title = `Lunch - $${lunchCharge.toFixed( 2 )}`;
+          lunchEvent.allDay = true;
+          lunchEvent.start = mealDate;
+          lunchEvent.end   = mealDate;
 
           mealEvents.push( lunchEvent );
       }
@@ -75,14 +68,29 @@ function getMeals() {
 
           depositEvent.allDay = true;
           depositEvent.color  = "green";
-          depositEvent.end    = moment( mealDate );
-          depositEvent.start  = moment( mealDate );
-          depositEvent.title  = "Deposit - $" + deposit.toFixed( 2 );
+          depositEvent.end    = mealDate;
+          depositEvent.start  = mealDate;
+          depositEvent.title  = `Deposit - $${deposit.toFixed( 2 )}`;
 
           mealEvents.push( depositEvent );
       }
 
+      if ( idx === 0 ) {
+          latestBalance = dailyBalance;
+      }
   });
+
+    const today = moment();
+    let balanceEvent = {};
+
+    balanceEvent.allDay = true;
+    balanceEvent.color = latestBalance < 5 ? 'red' : 'green';
+    balanceEvent.end = today;
+    balanceEvent.start = today;
+    balanceEvent.title = `Balance - $${latestBalance.toFixed(2)}`;
+
+    mealEvents.push(balanceEvent);
+
     return mealEvents;
 }
 
